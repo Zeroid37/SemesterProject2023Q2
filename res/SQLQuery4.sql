67f49735e725df89e0899b5c45bea51f5812e8c2
Zeroid37 <smajomanovic@gmail.com>
2023-05-27 01:09:02 +0200

    SQLQ update and populate

--- res/SQLQuery4.sql
+++ res/SQLQuery4.sql
@@ -10,7 +10,6 @@ go
 use holidayCenter;
 
 -- create tables
-
 create table ZipCity( 
 	zip varchar(16) not null,
 	city varchar(20) not null,
@@ -19,9 +18,9 @@ create table ZipCity(
 
 create table Address(
 	id int IDENTITY(1,1) not null,
-	zip_FK varchar(16) not null,
 	street varchar(16) not null,
 	houseNo varchar(10) not null,
+	zip_FK varchar(16) not null,
 	primary key (id),
 	foreign key (zip_FK) references ZipCity(zip)
 );
@@ -30,13 +29,13 @@ create table Person(
 	id int IDENTITY(1,1) not null,
 	fristName varchar(12) not null,
 	lastName varchar(20) not null,
-	addressId_FK varchar(12) not null,
+	addressId_FK int not null,
 	phone varchar(16) unique not null,
 	email varchar(30) unique not null,
 	userID varchar(16) unique not null,
 	password varchar(16) not null,
 	isAdmin bit,
-	type int not null,
+	type char not null,
 	primary key (email),
 	foreign key (addressId_FK) references Address(id)
 );
@@ -50,8 +49,8 @@ create table Instructor(
 
 create table Employee(
 	employeeNo int unique not null,
-	email_FK varchar(30) unique not null,
 	department varchar(16) not null,
+	email_FK varchar(30) unique not null,
 	primary key (employeeNo),
 	foreign key (email_FK) references Person(email)
 );
@@ -73,7 +72,7 @@ create table Profession(
 );
 
 create table InstructorProfession(
-	proessionID varchar(12) not null,
+	proessionID int not null,
 	instructorNo varchar(16) not null,
 	primary key (proessionID, instructorNo),
 	foreign key (proessionID) references Profession(id),
@@ -91,9 +90,7 @@ create table ApartmentDescription(
 	id int IDENTITY(1,1) not null,
 	hasBalcony bit not null,
 	floorNo int not null,
-	numberOfKingBeds int,
-	numberOfQueenBeds int,
-	numberOfNormalBeds int,
+	numberOfBeds int,
 	viewDescription varchar(16) not null,
 	primary key (id),
 );
@@ -101,7 +98,7 @@ create table ApartmentDescription(
 create table Apartment(
 	apartmentNo int unique not null,
 	apartmentType varchar(16) not null,
-	apartmentDescription_FK varchar(12) not null,
+	apartmentDescription_FK int not null,
 	primary key (apartmentNo),
 	foreign key (apartmentDescription_FK) references ApartmentDescription(id),
 );
@@ -109,7 +106,7 @@ create table Apartment(
 create table Booking(
 	id int IDENTITY(1,1) not null,
 	bookingNo int unique not null,
-	travelAgency varchar,
+	travelAgency varchar(20),
 	checkinDate datetime not null,
 	noOfNights int not null,
 	discount int,
@@ -117,17 +114,15 @@ create table Booking(
 	activityQuantityToday int,
 	employeeNo_FK int not null,
 	guestNo_FK int not null,
-	apartmentNo_FK int not null,
 	price money not null,
 	primary key (id),
 	foreign key (guestNo_FK) references Guest (guestNo),
-	foreign key (employeeNo_FK) references Employee(employeeNo),
-	foreign key (apartmentNo_FK) references Apartment(apartmentNo)
+	foreign key (employeeNo_FK) references Employee(employeeNo)
 );
 
 create table ApartmentBooking(
 	apartmentNo int not null,
-	bookingId varchar(12) not null,
+	bookingId int not null,
 	primary key (apartmentNo, bookingId),
 	foreign key (apartmentNo) references Apartment(apartmentNo),
 	foreign key (bookingId) references booking(id)
@@ -138,10 +133,10 @@ create table Reservation(
 	startDate datetime not null,
 	duration int not null,
 	attendeeAmount int not null,
-	professionID_FK varchar(12) not null,
+	professionID_FK int not null,
 	facilityNo_FK int not null,
 	instructorNo_FK varchar(16) not null,
-	bookingID_FK varchar(12) not null,
+	bookingID_FK int not null,
 	primary key (id),
 	foreign key (professionID_FK) references Profession(id),
 	foreign key (facilityNo_FK) references Facility(facilityNo),
@@ -154,14 +149,14 @@ create table Invoice(
 	invoiceNo int unique not null,
 	datePaid datetime not null,
 	sum int not null,
-	bookingID_FK varchar(12) not null,
+	bookingID_FK int not null,
 	primary key (id),
 	foreign key (bookingID_FK) references Booking(id)
 );
 
 create table Price(
 	id int IDENTITY(1,1) not null,
-	apartmentDescription_FK varchar(12) not null,
+	apartmentDescription_FK int not null,
 	dateFrom datetime not null,
 	price money not null,
 	primary key (id),
