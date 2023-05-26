daf75cec526896fe57e5473e7fe04170ac54a037
Zeroid37 <smajomanovic@gmail.com>
2023-05-26 06:06:00 +0200

    someModels

--- src/model/Apartment.java
+++ src/model/Apartment.java
@@ -1,5 +1,91 @@
 package model;
 
 public class Apartment {
+	private String apartmentNo;
+	private String apartmentType;
+	private boolean hasBalcony;
+	private String floorNo;
+	private int numberOfKingBeds;
+	private int numberOfQueenBeds;
+	private int numberOfNormalBeds;
+	private String viewDescription;
+	
+	public Apartment(String apartmentNo, String apartmentType, boolean hasBalcony, String floorNo, int numberOfKingBeds,
+					 int numberOfQueenBeds, int numberOfNormalBeds, String viewDescription) {
+		this.apartmentNo = apartmentNo;
+		this.apartmentType = apartmentType;
+		this.hasBalcony = hasBalcony;
+		this.floorNo = floorNo;
+		this.numberOfKingBeds = numberOfKingBeds;
+		this.numberOfQueenBeds = numberOfQueenBeds;
+		this.numberOfNormalBeds = numberOfNormalBeds;
+		this.viewDescription = viewDescription;
+	}
 
+	public String getApartmentNo() {
+		return apartmentNo;
+	}
+
+	public void setApartmentNo(String apartmentNo) {
+		this.apartmentNo = apartmentNo;
+	}
+
+	public String getApartmentType() {
+		return apartmentType;
+	}
+
+	public void setApartmentType(String apartmentType) {
+		this.apartmentType = apartmentType;
+	}
+
+	public boolean isHasBalcony() {
+		return hasBalcony;
+	}
+
+	public void setHasBalcony(boolean hasBalcony) {
+		this.hasBalcony = hasBalcony;
+	}
+
+	public String getFloorNo() {
+		return floorNo;
+	}
+
+	public void setFloorNo(String floorNo) {
+		this.floorNo = floorNo;
+	}
+
+	public int getNumberOfKingBeds() {
+		return numberOfKingBeds;
+	}
+
+	public void setNumberOfKingBeds(int numberOfKingBeds) {
+		this.numberOfKingBeds = numberOfKingBeds;
+	}
+
+	public int getNumberOfQueenBeds() {
+		return numberOfQueenBeds;
+	}
+
+	public void setNumberOfQueenBeds(int numberOfQueenBeds) {
+		this.numberOfQueenBeds = numberOfQueenBeds;
+	}
+
+	public int getNumberOfNormalBeds() {
+		return numberOfNormalBeds;
+	}
+
+	public void setNumberOfNormalBeds(int numberOfNormalBeds) {
+		this.numberOfNormalBeds = numberOfNormalBeds;
+	}
+
+	public String getViewDescription() {
+		return viewDescription;
+	}
+
+	public void setViewDescription(String viewDescription) {
+		this.viewDescription = viewDescription;
+	}
+	
+	
+	
 }
--- src/model/Booking.java
+++ src/model/Booking.java
@@ -1,5 +1,29 @@
 package model;
 
-public class Booking {
+import java.time.LocalDate;
 
+public class Booking {
+	private String bookingNo;
+	private String travelAgency;
+	private LocalDate dateStart;
+	private int noOfNights;
+	private int discount;
+	private boolean isDepositPaid;
+	private int activityQuantityToday;
+	private double price;
+	private Apartment apartment;
+	private Guest guest;
+	
+	
+	
+	public Booking(Apartment apartment, LocalDate dateStart, int noOfNights) {
+		this.apartment = apartment;
+		this.dateStart = dateStart;
+		this.noOfNights = noOfNights;
+		this.isDepositPaid = false;
+		this.activityQuantityToday = 0;
+	}
+	
+	
+	
 }
