ba5f5afb7917dde96fb529e24a151f8ac003dc50
Your Name <you@example.com>
2023-05-28 10:18:45 +0200

    logic måske

--- src/controller/BookingController.java
+++ src/controller/BookingController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import database.*;
@@ -8,56 +9,83 @@ import model.*;
 
 public class BookingController {
 	private Booking cBooking;
-	
-	public List<Apartment> searchForApartments(LocalDate dateStart, LocalDate dateEnd,
-			   								   double minPrice, double maxPrice, String apartmentType,
-			   								   int noOfBeds, int floorNo, boolean hasBalcony) throws DataAccessException {
+
+	public List<Apartment> searchForApartments(LocalDate dateStart, LocalDate dateEnd, double minPrice, double maxPrice,
+			String apartmentType, int noOfBeds, int floorNo, boolean hasBalcony) throws DataAccessException {
 		ApartmentController apartmentController = new ApartmentController();
-		List<Apartment> apartments = apartmentController.searchForApartments(minPrice, maxPrice, apartmentType, noOfBeds, floorNo, hasBalcony);
-		
+		List<Apartment> apartments = apartmentController.searchForApartments(minPrice, maxPrice, apartmentType,
+				noOfBeds, floorNo, hasBalcony);
+
+		for (int i = 0; i < apartments.size(); i++) {
+			Apartment a = apartments.get(i);
+			checkAvailable(dateStart, dateEnd, a);
+		}
+
 		return apartments;
 	}
-	
+
+	private boolean checkAvailable(LocalDate dateStart, LocalDate dateEnd, Apartment a) throws DataAccessException {
+		boolean res = true;
+		List<Booking> bookings = new ArrayList<>();
+
+		BookingDAO bookingDAO = new BookingDB();
+		bookings = bookingDAO.findBookingsByApartmentNo(a.getApartmentNo());
+
+		int s1 = convertDateToInt(dateStart);
+		int e1 = convertDateToInt(dateEnd);
+
+		for (int i = 0; i < bookings.size() && res; i++) {
+			int s2 = convertDateToInt(bookings.get(i).getDateStart());
+			int e2 = s2 + bookings.get(i).getNoOfNights();
+
+			if (s1 < e2 && s2 < e1) {
+				res = false;
+			}
+		}
+		return res;
+	}
+
 	public Booking startBooking(LocalDate dateStart, LocalDate dateEnd, String apartmentNo) throws DataAccessException {
 		ApartmentController apartmentController = new ApartmentController();
 		Apartment apartment = apartmentController.findApartmentByAparmentNo(apartmentNo);
-		
+
 		int noOfNights = calculateNoOfNights(dateStart, dateEnd);
 		cBooking = new Booking(apartment, dateStart, noOfNights);
 		cBooking.calculateAndSetPrice();
-		
+
 		LoginController loginController = new LoginController();
 		Employee e = loginController.getCurrEmployee();
 		cBooking.setEmployee(e);
 		return cBooking;
 	}
-	
-	public Guest createGuest(String firstName, String familyName, String street, String houseNo,
-                             String zip, String city, String phone, String email, String country) throws DataAccessException {
+
+	public Guest createGuest(String firstName, String familyName, String street, String houseNo, String zip,
+			String city, String phone, String email, String country, char type) throws DataAccessException {
 		PersonController personController = new PersonController();
-		Guest g = personController.createGuest(firstName, familyName, street, houseNo, zip, city, phone, email, country);
+		Guest g = personController.createGuest(firstName, familyName, street, houseNo, zip, city, phone, email, country,
+				type);
 		cBooking.setGuest(g);
 		return g;
 	}
-	
+
 	public boolean bookingConfirm() throws DataAccessException {
 		boolean res = false;
 		try {
-		DBConnection.getInstance().startTransaction();
-		BookingDAO bookingDAO = new BookingDB();
-		res = bookingDAO.addBookingToDB(cBooking);
-		DBConnection.getInstance().commitTransaction();
+			DBConnection.getInstance().startTransaction();
+			BookingDAO bookingDAO = new BookingDB();
+			res = bookingDAO.addBookingToDB(cBooking);
+			DBConnection.getInstance().commitTransaction();
 		} catch (Exception e) {
 			DBConnection.getInstance().rollbackTransaction();
 		}
 		return res;
 	}
-	
+
 	public boolean payDeposit(double amount, String bookingNo) throws DataAccessException {
 		BookingDAO bookingDAO = new BookingDB();
 		Booking booking = bookingDAO.findBookingByBookingNo(bookingNo);
 		boolean res = booking.isDepositValid(amount);
-		if(res) {
+		if (res) {
 			booking.setDepositPaid(true);
 			bookingDAO.updateBookingInDB(booking);
 		}
@@ -66,73 +94,21 @@ public class BookingController {
 
 	private int calculateNoOfNights(LocalDate dateStart, LocalDate dateEnd) {
 		int startDay = dateStart.getDayOfYear();
-		int endDay = dateStart.getDayOfYear();
+		int endDay = dateEnd.getDayOfYear();
 		int startYear = dateStart.getYear();
-		int endYear = dateStart.getDayOfYear();
+		int endYear = dateEnd.getDayOfYear();
 		int res;
-		
-		if(startYear == endYear) {
-			res = endDay - startDay;
-		} else {
-			int yearDiff = endYear - startYear;
-			res = endDay + yearDiff*365 - startDay;
-		}
-		
+
+		int yearDiff = endYear - startYear;
+		res = endDay + yearDiff * 365 - startDay;
+
 		return res;
 	}
-	
-	
-	
-	
-	
-	
-//	public void checkAvailable(LocalDate startDate, LocalDate endDate, Apartment a) {
-//		int yearStart = 2023;
-//		int dayOfYearStart = 165;
-//		int noOfNights = 20;
-//		
-//		int totalStart = (yearStart * 365) + dayOfYearStart; //738395
-//		int totalEnd = totalStart + noOfNights; //738415
-//		
-//	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+	private int convertDateToInt(LocalDate date) {
+		int year = date.getYear();
+		int day = date.getDayOfYear();
+		int total = year * 365 + day;
+		return total;
+	}
 }
--- src/gui/tui.java
+++ src/gui/tui.java
@@ -3,25 +3,92 @@ package gui;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import controller.LoginController;
+import database.BookingDAO;
+import database.BookingDB;
 import database.DBConnection;
+import database.DataAccessException;
+import model.Apartment;
+import model.Booking;
 
 public class tui {
 	public static void main(String[] args) {
 		
-		try {
-			DBConnection dbc = DBConnection.getInstance();
-			Connection con = dbc.getConnection();
-			
-			Statement s = con.createStatement();
-			ResultSet rs = s.executeQuery("select * from Apartment");
-			
-			while(rs.next()) {
-				System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
-			}
-		} catch (Exception e) {
-			
-		}
+//		try {
+//			DBConnection dbc = DBConnection.getInstance();
+//			Connection con = dbc.getConnection();
+//			
+//			Statement s = con.createStatement();
+//			ResultSet rs = s.executeQuery("select * from Apartment");
+//			
+//			while(rs.next()) {
+//				System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+//			}
+//		} catch (Exception e) {
+//			
+//		}
+		System.out.println(testCheck(150, 155)); //true
+		System.out.println(testCheck(155, 165)); //true
+		System.out.println(testCheck(150, 166)); //false
+		System.out.println(testCheck(130, 160)); //false
+		System.out.println(testCheck(166, 169)); //false
+		System.out.println(testCheck(170, 185)); //true
+		
 	}
+
+
+	private static boolean testCheck(int s1, int e1) { // x1 = s1, x2 = e1, y1= s2, y2 = e2
+		boolean res = true;
+		ArrayList<Integer> s2List = new ArrayList<>();
+		ArrayList<Integer> e2List = new ArrayList<>();
+
+		s2List.add(140);
+		e2List.add(150);
+		// 15
+		s2List.add(165);
+		e2List.add(170);
+		// 20
+		s2List.add(190);
+		e2List.add(205);
+		
+		for (int i = 0; i < s2List.size() && res ; i++) {
+			int s2 = s2List.get(i);
+			int e2 = e2List.get(i);
+			
+//			System.out.println(s1 + " <= " + e2 + " && " + e1 + " <= " + s2 + " = " + (s1 <= e2 && e1 <= s2));
+			
+			if(s1 < e2 && s2 < e1) {
+				res = false;
+			}
+		}
+		
+		return res;
+	}
+
+
+
 }
+//private boolean checkAvailable(LocalDate dateStart, LocalDate dateEnd, Apartment a) throws DataAccessException {
+//	boolean res = true;
+//	List<Booking> bookings = new ArrayList<>();
+//
+//	BookingDAO bookingDAO = new BookingDB();
+//	bookings = bookingDAO.findBookingsByApartmentNo(a.getApartmentNo());
+//
+//	int s1 = convertDateToInt(dateStart);
+//	int e1 = convertDateToInt(dateEnd);
+//	
+//	for (int i = 0; i < bookings.size() && res ; i++) {
+//		int s2 = convertDateToInt(bookings.get(i).getDateStart());
+//		int e2 = s2+bookings.get(i).getNoOfNights();
+//		
+//		if(!(s1 <= e2 && e1 <= s2)) {
+//			res = false;
+//		}
+//	}
+//	return res;
+//}
